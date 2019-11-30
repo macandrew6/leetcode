23c458a36095ad5ed47012fea1f631c1e7173a35
macandrew6 <mr.betterandrewmac@gmail.com>
2019-11-30 09:18:01 -0800

    fixes syntax errors

--- add_two_numbers.js
+++ add_two_numbers.js
@@ -33,21 +33,21 @@ let addTwoNumbers = (l1, l2) => {
   // list
   // return dummy head's next node
 
-  let dummyHead = new ListNode(0)
-  let curr = dummyHead
-  let carry = 0
-  while (l1 != null || l2 != null) {
-    sum = carry + (l1 ? l1.val : 0) + (l2 ? l2.val : 0)
-    carry = Math.floor(sum / 10)
-    curr.next = new ListNode(sum % 10)
-    curr = curr.next
-    l1 = (l1 ? l1.next : l1)
-    l2 = (l2 ? l2.next : l2)
+  let dummyHead = new ListNode(0);
+  let curr = dummyHead;
+  let carry = 0;
+  while (l1 !== null || l2 !== null) {
+    let sum = carry + (l1 ? l1.val : 0) + (l2 ? l2.val : 0);
+    carry = Math.floor(sum / 10);
+    curr.next = new ListNode(sum % 10);
+    curr = curr.next;
+    l1 = (l1 ? l1.next : l1);
+    l2 = (l2 ? l2.next : l2);
   }
   if (carry > 0) {
-    curr.next = new ListNode(carry)
+    curr.next = new ListNode(carry);
   }
-  return dummyHead.next
+  return dummyHead.next;
 };
 
 console.log(addTwoNumbers([1, 2, 3], [3, 2, 1]));

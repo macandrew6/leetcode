36bee43481ad9ccb05706d14a3c2236a248b94e3
macandrew6 <mr.betterandrewmac@gmail.com>
2019-11-30 09:29:54 -0800

    fixes variable bug

--- add_two_numbers.js
+++ add_two_numbers.js
@@ -36,8 +36,9 @@ let addTwoNumbers = (l1, l2) => {
   let dummyHead = new ListNode(0);
   let curr = dummyHead;
   let carry = 0;
+  let sum = 0;
   while (l1 !== null || l2 !== null) {
-    let sum = carry + (l1 ? l1.val : 0) + (l2 ? l2.val : 0);
+    sum = carry + (l1 ? l1.val : 0) + (l2 ? l2.val : 0);
     carry = Math.floor(sum / 10);
     curr.next = new ListNode(sum % 10);
     curr = curr.next;

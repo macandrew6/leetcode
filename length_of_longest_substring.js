951ba8dd22c1c2669706796a249c059120b26168
macandrew6 <mr.betterandrewmac@gmail.com>
2019-12-04 10:24:01 -0800

    solves algo problem

--- length_of_longest_substring.js
+++ length_of_longest_substring.js
@@ -26,5 +26,23 @@ function lengthOfLongestSubstring(str) {
   // save substring sequences into a hash
   // {'h': 1, 'he': 2, 'hel': 3, 'l': 1, 'le': 2}
   let subs = {};
-  
+  let start = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i - 1]) {
+      let sub = str.slice(start, i + 1);
+      subs[sub] = sub.length;
+    } else {
+      start = i;
+    }
+  }
+  console.log(subs);
+  let max = 0;
+  for (let key in subs) {
+    if(subs[key] > max) {
+      max = subs[key];
+    }
+  }
+  return max;
 }
+
+console.log(lengthOfLongestSubstring('bbbb'));
